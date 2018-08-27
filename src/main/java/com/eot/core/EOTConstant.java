26b37fb71e9aeb86206228d81dc52ff67e06982f
murari <murari@emproto.com>
2018-08-27 19:05:05 +0530

    constants for  commission created

--- src/main/java/com/eot/core/EOTConstant.java
+++ src/main/java/com/eot/core/EOTConstant.java
@@ -2,24 +2,24 @@ package com.eot.core;
 
 public interface EOTConstant {
 	
-	public String INVALID_USER = "invalid user";
+	public static String INVALID_USER = "invalid user";
 	
-	public String SUPERADMIN_LOGIN_SUCESS = "super admin login sucessfully";
+	public static String SUPERADMIN_LOGIN_SUCESS = "super admin login sucessfully";
 
-	public String COMMISSION_DOESNT_EXIST = "commission doesn't exist";
+	public static String COMMISSION_DOESNT_EXIST = "commission doesn't exist";
 
-	public String COMMISSION_ADDED_SUCCESSFULLY = "commission added successfully";
+	public static String COMMISSION_ADDED_SUCCESSFULLY = "commission added successfully";
 	
-	public String COMMISSION_UPDATED_SUCCESSFULLY = "commission updated successfully";
+	public static String COMMISSION_UPDATED_SUCCESSFULLY = "commission updated successfully";
 	
-	public String COMMISSION_DELETED_SUCCESSFULLY = "commission deleted successfully";
+	public static String COMMISSION_DELETED_SUCCESSFULLY = "commission deleted successfully";
 	
-	public String SUPERADMIN_DOESNOT_EXISTS="super admin does not exists";
+	public static String SUPERADMIN_DOESNOT_EXISTS="super admin does not exists";
 	
-	public String SUPERADMIN_DELETE_SUCCESSFULLY = "super admin deleted sucessfully";
+	public static String SUPERADMIN_DELETE_SUCCESSFULLY = "super admin deleted sucessfully";
 	
-	public String SUPERADMIN_ALREADY_EXISTS = "super admin already exits";
+	public static String SUPERADMIN_ALREADY_EXISTS = "super admin already exits";
 	
-	public Integer LOGINMAXATTEMPTS = new Integer(3);
+	public static Integer LOGINMAXATTEMPTS = new Integer(3);
 
 }
